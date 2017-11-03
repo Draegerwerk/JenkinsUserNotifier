16a43cb3fff4c097f14040790a13d811aeed83f5
Petersen, Yannik <Yannik.Petersen@draeger.com>
2017-11-03 11:14:06 +0100

    Add Cookie functionality with uuid via SHA-512 over information and date; Add MIT License to all fieles;

--- src/main/java/org/jenkinsci/plugins/JenkinsUserNotifierDecorator.java
+++ src/main/java/org/jenkinsci/plugins/JenkinsUserNotifierDecorator.java
@@ -1,3 +1,27 @@
+/*
+	The MIT License
+
+	Copyright (c) 2017, Draegerwerk AG & Co. KGaA , Yannik Petersen
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
 package org.jenkinsci.plugins;
 
 import hudson.Extension;
@@ -6,10 +30,19 @@ import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.StaplerRequest;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
 @Extension
 public class JenkinsUserNotifierDecorator extends PageDecorator{
 	private String information;
 	private String date;
+	private String uuid;
 
 	public JenkinsUserNotifierDecorator() {
 		super(JenkinsUserNotifierDecorator.class);
@@ -20,16 +53,69 @@ public class JenkinsUserNotifierDecorator extends PageDecorator{
 	public boolean configure(StaplerRequest req, JSONObject formData)
 			throws FormException {
 		information = formData.getString("information");
+
+		// TODO: check if the typed date is valid or not.
 		date = formData.getString("date");
+
+		// generate uuid for the cookie that is saved for any client that hides the notification bar
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			md.update(date.getBytes("UTF-8"));
+			byte[] bytes = md.digest(information.getBytes("UTF-8"));
+			StringBuilder sb = new StringBuilder();
+			for (byte aByte : bytes) {
+				sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+			}
+			uuid = sb.toString();
+		}
+		catch (NoSuchAlgorithmException | UnsupportedEncodingException e){
+			e.printStackTrace();
+		}
+
 		save();
 		return super.configure(req, formData);
 	}
 
+	/**
+	 * Getter for the saved Information that is displayed inside the notification bar
+	 * @return the saved information
+	 */
 	public String getInformation() {
 		return information;
 	}
 
+	/**
+	 * Getter for the saved date the notification will be shown
+	 * @return the saved date
+	 */
 	public String getDate() {
 		return date;
+	}
+
+	/**
+	 * Compares the current date with the date that was saved,
+	 * to evaluate if the Notification Banner should be loaded or not
+	 * @return true if current date is smaller than the saved one
+	 */
+	public boolean getNotificationActiveStatus() {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		Date date = null;
+
+		try {
+			date = simpleDateFormat.parse(this.date);
+		} catch (ParseException ex) {
+			System.out.println("Exception " + ex);
+		}
+		long currentEpoch = System.currentTimeMillis() / 1000;
+		assert date != null;
+		return currentEpoch < (date.getTime() / 1000);
+	}
+
+	/**
+	 * Getter for the notification uuid (timestamp)
+	 * @return uuid
+	 */
+	public String getNotificationUUID() {
+		return uuid;
 	}
 }
---
+++ src/main/webapp/JenkinsUserNotifierFunctions.js
@@ -0,0 +1,86 @@
+/*
+	The MIT License
+
+	Copyright (c) 2017, Draegerwerk AG & Co. KGaA , Yannik Petersen
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+*/
+
+// hides the NotificationBar and sets the cookie
+function hideNotifier(uuid, expiration_date) {
+	// hides the Notification Bar
+	hideBar();
+
+	// remember Hiding for current User as Cookie
+	setCookie(uuid, uuid, expiration_date);
+}
+
+// hides the Notification Bar
+function hideBar() {
+	var e = document.getElementById('notifier_container');
+
+	// get the div that defines the height of the breadcrumbBar
+	var bar = document.getElementById("breadcrumbBar");
+	var height_div = null;
+	var descendants = bar.getElementsByTagName('div');
+	if ( descendants.length ) {
+    	height_div = descendants[0];
+    	var current_height = Number(height_div.style.height.slice(0, -2));
+    	var remove_height = Number(e.offsetHeight);
+    	var new_height = current_height - remove_height;
+    	height_div.style.height = new_height + "px";
+    }
+
+	// hide the Notification Bar
+	e.style.display = 'none';
+}
+
+// sets a given cookie to a given value with the given expiration date (as string "MM/dd/yyyy")
+function setCookie(name, value, expiration_date) {
+	var date_parts = expiration_date.split('/');
+	var expiration_date = new Date(date_parts[2],date_parts[0]-1,date_parts[1]);
+	expiration_date.setDate(expiration_date.getDate() + 1);
+	document.cookie = name + "=" + value + "; expires=" + expiration_date.toUTCString() + "; path=/";
+}
+
+// returns the value of the given cookie if it exists
+function getCookie(c_name) {
+    var name = c_name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+// checks if the given cookie exists or not
+function cookieExists(uuid) {
+	var cookie = getCookie(uuid);
+	if (cookie == null || cookie == "") {
+		return false;
+	}
+	return true;
+}
